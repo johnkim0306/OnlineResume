34e1f45f44660ab4b4f0a5d02c2b6460e941cbea
John Kim <mkcokr2176@gmail.com>
2023-10-01 20:34:25 -0700

    Update project section

--- src/components/project/data.js
+++ src/components/project/data.js
@@ -1,9 +1,9 @@
 export const projects = [
 {
     title: "Covid React tracker",
-    date: "2022.07 ~ 2022.08",
+    date: "2023.09 ~ 2023.10",
     skills: [
-    "React",
+    "Next.js",
     "JavaScript",
     "MaterialUI",
     "Sass",
@@ -15,10 +15,25 @@ export const projects = [
         "Visualizing live data for the Covid-19 cases around the world. Fetch REST API and used React, Material-UI, and Axios to build a responsive web application.",
         "Implemented a clustering effect on a map using a JavaScript library Leaflet. Additionally, it followed a naming methodology B.E.M with SASS.",
         "Used a JavaScript library Leaflet to implement a map with a clustering effect.",
+        "Developed a responsive web application to track and visualize live data of Covid-19 cases worldwide using Next.js (React) and Material-UI. The application fetches real-time data via a REST API and utilizes a custom pre-trained machine learning model to assess potential Covid-19 cases among users.",
         "Interacted with a web API to fetch data and render it.",
         "Constructed componnets based on atomic design system.",
         "Confirmed cross browser testing.",
         "Implemented responsive website using media queries.",
+    ],
+},
+{
+    title: "SimpleApp",
+    date: "2023.02 ~ 2023.0",
+    skills: [
+    "React",
+    "Sass",
+    ],
+    website:"https://simplenoteapp.netlify.app/ ",
+    github: "https://github.com/johnkim0306/TodoList",
+    about: "Note taking app with voice-to-text enabled",
+    contents: [
+        "Developed a user-friendly memo application using React that leverages the JavaScript Web Speech API to enable voice-to-text memo creation and incorporates local storage functionality for saving and retrieving memos, ensuring data persistence."
     ],
 },
 {
@@ -27,7 +42,7 @@ export const projects = [
     skills: ["Vue.js", "Express.js", "MariaDB", "Bootstrap"],
     github: "https://github.com/johnkim0306/commerce",
     about:
-    "E-commerce website that ",
+    "E-commerce website with responsive development ",
     contents: [
         "Developed a full-stack web application using Express.js covering CRUD operation, authentication, and purchasing. Used Vue.js and Bootstrap as a frontend development framework enabling responsive development.",
         "Implement vue3-google-oauth2 to retrieve data from the user and used Vuex to store this data. Used Vuex-PersistedState to store state in local storage.",
