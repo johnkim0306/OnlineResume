8dc5ed93386613200bf80c3dd6d841b42e5a5a9a
John Kim <mkcokr2176@gmail.com>
2024-04-22 20:57:53 -0700

    Update work experience aligning with linkedin one

--- my-app/src/components/work/sliderData.js
+++ my-app/src/components/work/sliderData.js
@@ -8,6 +8,8 @@ const SliderData = [
       desc1: "Created a comprehensive website that seamlessly tracks customer information and the orders across all the B2B platforms. Automated the generation of invoices and product labels for enhanced efficiency.",
       desc2: "Developed and implemented a custom PHP-based module to enhance functionality in the back office and improve user experience for the e-commerce website, leading to a 12% increase in daily active users.",
       desc3: "Implemented new components using modern technologies such as Nest.js, Apollo, React TypeScript, GraphQL, and TypeORM to optimize data retrieval and manipulation.",
+      desc4: "Designed and implemented data calling feature for a web application to connect with Twilio for devices and agent communication increasing mobile traffic by 23%, contributing to the highest sales figures of 2023",
+      desc5: "Built and optimized RESTful APIs in Node.js for B2B order management, invoicing, and communication processes,serving over 5,000 clients per month, resulting in a 30% increase in data processing speed and a 25% reduction in API response time",
       img: "https://99designs-blog.imgix.net/blog/wp-content/uploads/2018/10/attachment_100040756-e1538485934255.jpeg?auto=format&q=60&fit=max&w=930",
     },
     {
